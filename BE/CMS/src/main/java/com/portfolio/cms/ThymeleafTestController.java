0911c8cb2fd71dd4059d369fccd708578078ca46
JeahaOh <jeaha1217@gmail.com>
2023-05-07 13:18:14 +0900

    Logback 사용
    application.yml 설정 한방에 끝

--- BE/CMS/src/main/java/com/portfolio/cms/ThymeleafTestController.java
+++ BE/CMS/src/main/java/com/portfolio/cms/ThymeleafTestController.java
@@ -1,12 +1,15 @@
 package com.portfolio.cms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class ThymeleafTestController {
     @RequestMapping("/test")
     public String test() {
+        log.debug("LOG");
         return "main/main";
     }
 }
