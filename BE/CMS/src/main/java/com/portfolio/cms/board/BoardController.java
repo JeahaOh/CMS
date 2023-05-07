300b63a5391e80761681a12bede17fb240e61a59
JeahaOh <jeaha1217@gmail.com>
2023-05-07 16:42:34 +0900

    부트스트랩 설정 및 게시판 html 생성

---
+++ BE/CMS/src/main/java/com/portfolio/cms/board/BoardController.java
@@ -0,0 +1,26 @@
+package com.portfolio.cms.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @GetMapping({"", "/"})
+    public String list() {
+        return "/board/list";
+    }
+    
+    @GetMapping("/write")
+    public String write() {
+        return "/board/write";
+    }
+    
+    @GetMapping("/update")
+    public String update() {
+        return "/board/update";
+    }
+
+}
